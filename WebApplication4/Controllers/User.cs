88b9075ddafd12a5dadad7777c3b91d508f5acb3
sonmc90 <sonmc90@gmail.com>
2021-10-26 22:31:00 +0700

    Structure project simple

---
+++ WebApplication4/Controllers/User.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Controllers
+{
+    public class User : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username.Equals("admin") && password == "123123")
+            {
+                return RedirectToAction("Index","Product");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
